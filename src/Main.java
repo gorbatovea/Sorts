5376eb80be2cc3e3061f4ac9544a3a066abf5411
gorbatov <gorbatov.eg@gmail.com>
2017-11-27 15:14:46 +0300

    Bugs fixed in LSD sort Implemented SimpleInteger struct LSD sort checked on SimpleInteger lists Fixed few issues and warnings

--- src/Main.java
+++ src/Main.java
@@ -19,32 +19,9 @@ public class Main{
             }
             System.out.println();
             System.out.println();
-            return false;
         }
         else
             System.out.println(sort.getClass() + ":OK");
-        return true;
-    }
-
-    public static <S extends AbstractSortOnComparisons<SimpleString>> boolean checkSort(SimpleString[] list, S sort) {
-        SimpleString[] sortedList = Arrays.copyOf(list, list.length);
-        sort.sort(sortedList);
-
-        if (!SortUtils.isArraySorted(sortedList)) {
-            System.out.print("Array: ");
-            for (SimpleString aList : list) System.out.print(aList + " ");
-            System.out.println();
-            System.out.print(sort.getClass() + ": ");
-            for (int i = 0; i < list.length; i++) {
-                System.out.print(sortedList[i] + " ");
-            }
-            System.out.println();
-            System.out.println();
-            return false;
-        }
-        else
-            System.out.println(sort.getClass() + ":OK");
-        return true;
     }
 
     public static void main(String[] args) {
